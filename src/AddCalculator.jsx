4034d01ce3b403f24e28884c8f88139474cc48ac
Imane Allay <iallay@xpanxion.com>
2023-05-30 10:34:16 -0400

    Add Ex12

--- src/AddCalculator.jsx
+++ src/AddCalculator.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 
 export const AddCalculator = () => {
+    // a state that has a variable state and a setter
+    let[total,setTotal]=useState(0)
+    
+    // this is a function that has 1 parameter and we can give 
+    // the value that we want inside this function
+    const onhandleClick=(number)=>{
+        setTotal(total+=number)
 
+    }
     return (
         <>
-            <button>Add 1</button>
-            <button>Add 2</button>
-            <button>Add 3</button>
+            <button onClick={()=>onhandleClick(1)}>Add 1</button>
+            <button onClick={()=>onhandleClick(2)}>Add 2</button>
+            <button onClick={()=>onhandleClick(3)}>Add 3</button>
             <br></br>
-            Total: 
+            {/* display my total */}
+            Total:{total}
         </>
     )
 }
