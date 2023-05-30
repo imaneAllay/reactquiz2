1f1eeecd8228aa8b8fc3959ee1c6d67ec318017e
Imane Allay <iallay@xpanxion.com>
2023-05-30 10:25:52 -0400

    Add Ex11

--- src/NumberBuilder.jsx
+++ src/NumberBuilder.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const NumberBuilder = () => {
+    // Creating 2 states
+    let[number,setNumber]=useState(0)
+    let[str,setStr]=useState("")
+    
+    // The function that handle Addnumber
+    const onHandleClick= ()=>{
+        setNumber(++number)
+        setStr(str+=number +" ")
+    }
+   // The function that Reset 
+    const onHandleReset= ()=>{
+        setNumber(0)
+        setStr("")
+    }
+// comment
 
     return (
         <>
-            <button>Add Number</button>
-            <button>Reset</button>
+            <button onClick={()=>onHandleClick()}>Add Number</button>
+            <button onClick={()=>onHandleReset()}>Reset</button>
             <br></br>
-            Numbers: 
+            Numbers: {str}
         </>
     )
 }
